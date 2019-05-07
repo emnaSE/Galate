0ce7555659ac55d56cd1dc7fb3ffc762a25a8f58
marwaselmi <selmi_marwa@yahoo.fr>
2019-05-07 15:21:27 +0100

    add admin router

--- src/router/router.js
+++ src/router/router.js
@@ -1,32 +1,32 @@
-'use strict';
-
 const router = require('express').Router();
-
-
-
-
-
-const DISCOUNT=5/100;
 router.use((req, res, next) => {
-  res.payload = {};
-  res.payload.discount=DISCOUNT;
-  next();
-});
-
-
-
-
-
-
-  /**
- * Last route, handles not found.
- */
-router.use((req, res) => {
-if (req.xhr)
-  return res.status(404).json({
-    message: 'Could not find the resource you were looking for',
+    res.payload = {};
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
   });
-return res.status(404).render('not-found');
-});
+
+
+
+
+
+
+
+router.use('/admin', require('./adminRouter'));
+//router.use('/order', require('./order'));
+//router.use('/client', require('./client'));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
