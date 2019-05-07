15eabb8decdf5bb368f4ccb4032c60866dff032c
mouadh <mouadh.yahyaoui1993@gmail.com>
2019-05-07 09:42:23 +0100

    template backoffice

---
+++ frantend/src/app/pages/pages-menu.ts
@@ -0,0 +1,39 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'Categorie',
+    icon: 'nb-home',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'Sous Categorie',
+    icon: 'nb-star',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'Test',
+    icon: 'nb-e-commerce',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'Ecole',
+    icon: 'ion-bookmark',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'User',
+    icon: 'nb-compose',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'FEATURES',
+    group: true,
+  },
+
+];
---
+++ frantend/src/app/pages/pages.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { PagesRoutingModule } from './pages-routing.module';
+import { ThemeModule } from '../@theme/theme.module';
+import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+
+const PAGES_COMPONENTS = [
+  PagesComponent,
+];
+
+@NgModule({
+  imports: [
+    PagesRoutingModule,
+    ThemeModule,
+    DashboardModule,
+    MiscellaneousModule
+  ],
+  declarations: [
+    ...PAGES_COMPONENTS,
+  ],
+})
+export class PagesModule {
+}
