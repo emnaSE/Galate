f4c6b833e053c6f49ccd0e6b0c7b4ce4cb579310
Gahlouzi Ameni <gahlouzi.ameni@gmail.com>
2019-05-14 13:02:45 +0100

    add front-office project

---
+++ front-office/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {TooltipModule} from "ngx-bootstrap";
+import { StarterComponent } from './starter/starter.component';
+import { StarterHeaderComponent } from './starter/starter-header/starter-header.component';
+import { StarterFooterComponent } from './starter/starter-footer/starter-footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StarterComponent,
+    StarterHeaderComponent,
+    StarterFooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TooltipModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
