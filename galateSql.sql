02c67042ea001bc3e7c8ac1e72fe1517e77bcbe9
marwaselmi <selmi_marwa@yahoo.fr>
2019-05-15 09:54:48 +0100

    add result column to manuel_answer

--- galateSql.sql
+++ galateSql.sql
@@ -193,3 +193,4 @@ create table manuel_answer(
 );
 alter table manuel_answer add column etallonage_result bigint;
 alter table answer add column ordre bigint;
+alter table manuel_answer add column result bigint;
