b812243e9406e9d507f0acefa7f834622d04a964
RaddaouiAmeni <raddaouiameni59@gmail.com>
2019-05-15 14:04:59 +0100

    ajouter member

--- front-office/src/app/pages/register/register.component.ts
+++ front-office/src/app/pages/register/register.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
-
-
+import {Router, ActivatedRoute} from "@angular/router";
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { RegisterService } from './register.service';
 
 
 @Component({
@@ -9,13 +9,58 @@ import {Router} from "@angular/router";
   templateUrl: './register.component.html',
 })
 export class RegisterComponent  implements OnInit{
-  pageActuel: number =1;
+
+  id:any;
+  editMode=false;
+  addForm:FormGroup;
+  submitted = false;
+  constructor(private router:Router,
+              private registerService:RegisterService,
+              private formBuilder:FormBuilder,
+              private activateRouter:ActivatedRoute){
+
+
+  }
+
+  ngOnInit() {
+    this.id = this.activateRouter.snapshot.params['id'];
+    if(this.id){
+
+      this.editMode=true;
+      
+    }
+
+    this.addForm=this.formBuilder.group({
+      name: new FormControl('', [Validators.required]),
+
+
+    });
 
 
 
 
-    constructor(private router:Router){ }
-  ngOnInit() { }
 
+  }
+  get formValidate(){
+    return this.addForm.controls;
+  }
+
+  onSubmit(){
+    this.submitted=true;
+    //this.editMode=true;
+    
+      if(this.addForm.valid){
+        this.registerService.addMember(this.addForm.value).subscribe(
+          data=>{
+            alert("ajouter avec succes")
+            this.router.navigate(['pages/register'])
+          },err=>{
+            console.log(err);
+          }
+        )
+      }
+    }
+
+  }
   
-}
+
---
+++ front-office/src/app/pages/register/register.model.ts
@@ -0,0 +1,16 @@
+
+
+export interface Class {
+    id?:number;
+    firstname?: string;
+    lastname?: string;
+    email?: string;
+    age?: string;
+    pseudo?: string;
+    password?: string;
+    civility?: string;
+    id_clazz?:any;
+    id_school?:any;
+
+
+  }
