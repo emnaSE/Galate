8ac02f8ca9d878c59869d206f554b286458c1166
fedia <selmi_marwa@yahoo.fr>
2019-05-20 12:32:39 +0100

    resoudre conflit

--- nodejs/src/router/adminRouter.js
+++ nodejs/src/router/adminRouter.js
@@ -7,7 +7,7 @@ var options = {
     inflate: true,
     limit: '100kb',
     type: 'application/octet-stream'
-};
+  };
 const bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -19,260 +19,260 @@ router.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader('Access-Control-Allow-Credentials', true)
     next();
-});
+  });
 
 
 
 var getRawBody = require('raw-body')
 router.use(bodyParser.urlencoded({ extended: true }));
-router.get('/getAllCategories', urlencodedParser, (req, res, next) =>
-    adminController.getAllCategories(req)
-        .then(categories => {
-            res.send(categories);
-        })
-        .catch(next));
+router.get('/getAllCategories', urlencodedParser, (req, res, next) => 
+ adminController.getAllCategories(req)
+.then(categories => {
+  res.send(categories);
+})
+.catch(next));
 
-router.get('/getCategoryById', urlencodedParser, (req, res, next) =>
-    adminController.getCategoryById(req)
-        .then(categories => {
-            res.send(categories);
-        })
-        .catch(next));
+router.get('/getCategoryById', urlencodedParser, (req, res, next) => 
+ adminController.getCategoryById(req)
+.then(categories => {
+  res.send(categories);
+})
+.catch(next));
 
 
-router.post('/createCategory', (req, res, next) =>
-    memberController.getRawBody(req)
-        .then(category => {
-            res.payload.category = category;
-            return adminController.createCategory(category)
-        })
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
-
+  router.post('/createCategory', (req, res, next) =>
+  memberController.getRawBody(req)
+  .then(category => {
+      res.payload.category = category;
+      return adminController.createCategory(category)
+  })
+  .then(msg => {
+      res.send(msg);
+  })
+  .catch(next));
+  
 router.post('/updateCategoryById', (req, res, next) =>
-    memberController.getRawBody(req)
-        .then(category => {
-            return adminController.updateCategory(req, category)
-        })
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+memberController.getRawBody(req)
+.then(category => { 
+    return adminController.updateCategory(req, category)
+})
+.then(msg => {
+    res.send(msg);
+})
+.catch(next));
 
 router.post('/deleteCategoryById', (req, res, next) =>
-    adminController.deleteCategory(req)
+adminController.deleteCategory(req)
 
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+.then(msg => {
+    res.send(msg);
+})
+.catch(next));
 
 //class 
 
-router.get('/getAllClasses', urlencodedParser, (req, res, next) =>
-    adminController.getAllClasses(req)
-        .then(classes => {
-            res.send(classes);
-        })
-        .catch(next));
+router.get('/getAllClasses', urlencodedParser, (req, res, next) => 
+ adminController.getAllClasses(req)
+.then(classes => {
+  res.send(classes);
+})
+.catch(next));
 
-router.get('/getAllClassesBySchool', urlencodedParser, (req, res, next) =>
-    adminController.getAllClassesByIdSchool(req)
-        .then(classes => {
-            res.send(classes);
-        })
-        .catch(next));
-router.get('/getClassById', urlencodedParser, (req, res, next) =>
-    adminController.getClassById(req)
-        .then(classes => {
-            res.send(classes);
-        })
-        .catch(next));
+router.get('/getAllClassesBySchool', urlencodedParser, (req, res, next) => 
+ adminController.getAllClassesByIdSchool(req)
+.then(classes => {
+  res.send(classes);
+})
+.catch(next));
+router.get('/getClassById', urlencodedParser, (req, res, next) => 
+ adminController.getClassById(req)
+.then(classes => {
+  res.send(classes);
+})
+.catch(next));
 
 router.post('/createClass', (req, res, next) =>
-    memberController.getRawBody(req)
-        .then(clazz => {
-            res.payload.clazz = clazz;
-            return adminController.createClazz(clazz)
-        })
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+memberController.getRawBody(req)
+.then(clazz => {
+    res.payload.clazz = clazz;
+    return adminController.createClazz(clazz)
+})
+.then(msg => {
+    res.send(msg);
+})
+.catch(next));
 
 
 
 router.post('/updateClassById', (req, res, next) =>
-    memberController.getRawBody(req)
-        .then(clazz => {
-            return adminController.updateClazz(req, clazz)
-        })
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+memberController.getRawBody(req)
+.then(clazz => {
+    return adminController.updateClazz(req, clazz)
+})
+.then(msg => {
+    res.send(msg);
+})
+.catch(next));
 
 
 router.post('/deleteClassById', (req, res, next) =>
-    adminController.deleteClazzById(req)
+adminController.deleteClazzById(req)
 
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+.then(msg => {
+    res.send(msg);
+})
+.catch(next));
 router.post('/deleteClassByIdSchool', (req, res, next) =>
-    adminController.deleteClazzByIdSchool(req)
+adminController.deleteClazzByIdSchool(req)
 
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+.then(msg => {
+    res.send(msg);
+})
+.catch(next));
 //school
 
-router.get('/getSchoolById', urlencodedParser, (req, res, next) =>
-    adminController.getSchoolById(req)
-        .then(schools => {
-            res.send(schools);
-        })
-        .catch(next));
-router.get('/getAllSchools', urlencodedParser, (req, res, next) =>
-    adminController.getAllSchools(req)
-        .then(schools => {
-            res.send(schools);
-        })
-        .catch(next));
+router.get('/getSchoolById', urlencodedParser, (req, res, next) => 
+ adminController.getSchoolById(req)
+.then(schools => {
+  res.send(schools);
+})
+.catch(next));
+router.get('/getAllSchools', urlencodedParser, (req, res, next) => 
+ adminController.getAllSchools(req)
+.then(schools => {
+  res.send(schools);
+})
+.catch(next));
 
-router.post('/createSchool', (req, res, next) =>
-    memberController.getRawBody(req)
-        .then(school => {
-            return adminController.createSchool(school)
-        })
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
-
+  router.post('/createSchool', (req, res, next) =>
+  memberController.getRawBody(req)
+  .then(school => {
+      return adminController.createSchool(school)
+  })
+  .then(msg => {
+      res.send(msg);
+  })
+  .catch(next));
+  
 router.post('/updateSchoolById', (req, res, next) =>
-    memberController.getRawBody(req)
-        .then(school => {
-            return adminController.updateSchool(req, school)
-        })
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+memberController.getRawBody(req)
+.then(school => {
+    return adminController.updateSchool(req, school)
+})
+.then(msg => {
+    res.send(msg);
+})
+.catch(next));
 
 router.post('/deleteSchoolById', (req, res, next) =>
-    adminController.deleteSchool(req)
+adminController.deleteSchool(req)
 
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+.then(msg => {
+    res.send(msg);
+})
+.catch(next));
 
 //subcategory
-router.get('/getAllSubcategories', urlencodedParser, (req, res, next) =>
-    adminController.getAllSubcategories(req)
-        .then(subcategories => {
-            res.send(subcategories);
-        })
-        .catch(next));
-router.get('/getSubcategoryById', urlencodedParser, (req, res, next) =>
-    adminController.getSubcategoryById(req)
-        .then(subcategories => {
-            res.send(subcategories);
-        })
-        .catch(next));
+router.get('/getAllSubcategories', urlencodedParser, (req, res, next) => 
+ adminController.getAllSubcategories(req)
+.then(subcategories => {
+  res.send(subcategories);
+})
+.catch(next));
+router.get('/getSubcategoryById', urlencodedParser, (req, res, next) => 
+ adminController.getSubcategoryById(req)
+.then(subcategories => {
+  res.send(subcategories);
+})
+.catch(next));
 
-router.get('/getAllSubcategoriesByCategory', urlencodedParser, (req, res, next) =>
-    adminController.getAllSubcategoriesByCategory(req)
-        .then(subcategories => {
-            res.send(subcategories);
-        })
-        .catch(next));
+router.get('/getAllSubcategoriesByCategory', urlencodedParser, (req, res, next) => 
+ adminController.getAllSubcategoriesByCategory(req)
+.then(subcategories => {
+  res.send(subcategories);
+})
+.catch(next));
 router.get('/getAllSubcategoriesByTestId', urlencodedParser, (req, res, next) =>
-    adminController.getAllSubcategoriesByIdTest(req)
-        .then(subcategories => {
-            res.send(subcategories);
-        })
-        .catch(next));
+adminController.getAllSubcategoriesByIdTest(req)
+    .then(subcategories => {
+        res.send(subcategories);
+    })
+    .catch(next));
 
 router.post('/createSubcategory', (req, res, next) =>
-    memberController.getRawBody(req)
-        .then(Subcategory => {
-            res.payload.Subcategory = Subcategory;
-            return adminController.createSubCategory(Subcategory)
-        })
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+memberController.getRawBody(req)
+.then(Subcategory => {
+    res.payload.Subcategory = Subcategory;
+    return adminController.createSubCategory(Subcategory)
+})
+.then(msg => {
+    res.send(msg);
+})
+.catch(next));
 
 router.post('/updateSubcategoryById', (req, res, next) =>
-    memberController.getRawBody(req)
-        .then(Subcategory => {
-            return adminController.updateSubCategory(req, Subcategory)
-        })
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+memberController.getRawBody(req)
+.then(Subcategory => {
+  return adminController.updateSubCategory(req, Subcategory)
+})
+.then(msg => {
+  res.send(msg);
+})
+.catch(next));
 
 router.post('/deleteSubcategoryById', (req, res, next) =>
-    adminController.deleteSubCategoryById(req)
+adminController.deleteSubCategoryById(req)
 
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+.then(msg => {
+  res.send(msg);
+})
+.catch(next));
 
 router.post('/deleteSubcategroies', (req, res, next) =>
-    adminController.deleteSubCategorysByIdCategory(req)
+adminController.deleteSubCategorysByIdCategory(req)
 
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+.then(msg => {
+  res.send(msg);
+})
+.catch(next));
 
 //question
 
 router.post('/createQuestion', (req, res, next) =>
-    adminController.getRawBody(req)
-        .then(question => {
-            res.payload.question = question;
-            return adminController.createQuestion(question)
-        })
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+adminController.getRawBody(req)
+.then(question => {
+    res.payload.question = question;
+    return adminController.createQuestion(question)
+})
+.then(msg => {
+    res.send(msg);
+})
+.catch(next));
 
 
 router.post('/updateQuestionById', (req, res, next) =>
-    adminController.getRawBody(req)
-        .then(question => {
-            return adminController.updateQuestion(req, question)
-        })
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+adminController.getRawBody(req)
+.then(question => {
+    return adminController.updateQuestion(req, question)
+})
+.then(msg => {
+    res.send(msg);
+})
+.catch(next));
 
-router.get('/deleteQuestionById', urlencodedParser, (req, res, next) =>
-    adminController.deleteQuestion(req)
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+router.get('/deleteQuestionById', urlencodedParser, (req, res, next) => 
+adminController.deleteQuestion(req)
+.then(msg => {
+    res.send(msg);
+})
+.catch(next));
 
-router.get('/getAllQuestion', urlencodedParser, (req, res, next) =>
-    adminController.getAllQuestion(req)
-        .then(questions => {
-            res.send(questions);
-        })
-        .catch(next));
+router.get('/getAllQuestion', urlencodedParser, (req, res, next) => 
+adminController.getAllQuestion(req)
+.then(questions => {
+  res.send(questions);
+})
+.catch(next));
 
 router.get('/getAllQuestionsByIdSubcategory', urlencodedParser, (req, res, next) =>
     adminController.getAllQuestionsByIdSubcategory(req)
@@ -294,12 +294,12 @@ router.get('/getAllQuestionsByIdSubcategory', urlencodedParser, (req, res, next)
 
 
 
-router.get('/getQuestionById', urlencodedParser, (req, res, next) =>
-    adminController.getQuestionById(req)
-        .then(question => {
-            res.send(question);
-        })
-        .catch(next));
+router.get('/getQuestionById', urlencodedParser, (req, res, next) => 
+ adminController.getQuestionById(req)
+.then(question => {
+  res.send(question);
+})
+.catch(next));
 
 //Answer
 
@@ -523,52 +523,52 @@ memberController.getRawBody(req)
 
 
 router.post('/deleteTestById', (req, res, next) =>
-    adminController.deleteTest(req)
+adminController.deleteTest(req)
 
-        .then(msg => {
-            res.send(msg);
-        })
-        .catch(next));
+.then(msg => {
+    res.send(msg);
+})
+.catch(next));
 
 
 
 router.post('/affectCategoriesToTest', (req, res, next) => adminController.
-    getRawBody(req)
-    .then(affectation => {
-        var affectation = JSON.parse(affectation);
-        var testId = req.query.testId;
-        res.payload.testId = testId;
-        res.payload.categoriesList = affectation.categories;
-        return adminController.RemoveAffectationCategoriesToTest(testId, affectation.categories);
-    })
-    .then(response => {
-        return adminController.AffectCategoriesToTest(res.payload.testId, res.payload.categoriesList);
-    })
-    .then(response => {
-        res.send(response);
-    })
-    .catch(next));
+getRawBody(req)
+.then(affectation => {
+  var affectation = JSON.parse(affectation);
+  var testId = req.query.testId;
+  res.payload.testId = testId;
+  res.payload.categoriesList = affectation.categories;
+  return adminController.RemoveAffectationCategoriesToTest(testId, affectation.categories);
+})
+.then(response => {
+  return adminController.AffectCategoriesToTest(res.payload.testId, res.payload.categoriesList);
+})
+.then(response => {
+  res.send(response);
+})
+.catch(next));
 
 
 router.post('/affectSubcategoriesToTest', (req, res, next) => adminController.
 
-    getRawBody(req)
-    .then(affectation => {
-        var affectation = JSON.parse(affectation);
-        var testId = req.query.testId;
-        res.payload.testId = testId;
-        res.payload.subcategoriesList = affectation.subcategories;
-        res.payload.questionsNumber = affectation.questionsNumber;
-        res.payload.wording = affectation.wording;
-        return adminController.RemoveAffectationSubcategoriesToTest(testId, affectation.subcategories);
-    })
-    .then(response => {
-        return adminController.AffectSubcategoriesToTest(res.payload.testId, res.payload.subcategoriesList, res.payload.questionsNumber, res.payload.wording);
-    })
-    .then(response => {
-        res.send(response);
-    })
-    .catch(next));
+getRawBody(req)
+.then(affectation => {
+  var affectation = JSON.parse(affectation);
+  var testId = req.query.testId;
+  res.payload.testId = testId;
+  res.payload.subcategoriesList = affectation.subcategories;
+  res.payload.questionsNumber = affectation.questionsNumber;
+  res.payload.wording = affectation.wording;
+  return adminController.RemoveAffectationSubcategoriesToTest(testId, affectation.subcategories);
+})
+.then(response => {
+  return adminController.AffectSubcategoriesToTest(res.payload.testId, res.payload.subcategoriesList, res.payload.questionsNumber, res.payload.wording);
+})
+.then(response => {
+  res.send(response);
+})
+.catch(next));
 
 
 router.post('/duplicateTest', (req, res, next) =>
@@ -698,12 +698,12 @@ getQuestionById(req)
 
 
 //test by class member date
-router.get('/getTestClassDateMember', urlencodedParser, (req, res, next) =>
-    adminController.getTestClassDateMember(req)
-        .then(tests => {
-            res.send(tests);
-        })
-        .catch(next));
+router.get('/getTestClassDateMember', urlencodedParser, (req, res, next) => 
+adminController.getTestClassDateMember(req)
+.then(tests => {
+  res.send(tests);
+})
+.catch(next));
 
 //test fait 
 
