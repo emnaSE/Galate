b6fefb761b58cc07a76e29d1c273310c3846a62d
Gahlouzi Ameni <gahlouzi.ameni@gmail.com>
2019-09-13 16:49:42 +0100

    report changes

--- galateProject/src/main/java/galateProject/ConnectionConfig.java
+++ galateProject/src/main/java/galateProject/ConnectionConfig.java
@@ -1,8 +1,8 @@
 package galateProject;
 
 public class ConnectionConfig {
-	public static String connectionString="jdbc:mysql://localhost:3306/galate6?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	public static String connectionString="jdbc:mysql://localhost:3306/galate?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	public static String user="root";
-	public static String password="wqtqVSROVpdhilqI00";
+	public static String password="tbEsNGfMcF8Htt7n";
 
 }
